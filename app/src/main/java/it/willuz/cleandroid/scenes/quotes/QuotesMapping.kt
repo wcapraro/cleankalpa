54208d7e27fcfa3b728aba8fb6c0d4f5cc74cbd3
Wiliam <wiliam.capraro@kalpa.it>
2021-07-05 11:58:23 +0200

    fix: quotes mapping author display name

--- app/src/main/java/it/willuz/cleandroid/scenes/quotes/QuotesMapping.kt
+++ app/src/main/java/it/willuz/cleandroid/scenes/quotes/QuotesMapping.kt
@@ -1,10 +1,11 @@
 package it.willuz.cleandroid.scenes.quotes
 
 import it.willuz.cleandroid.domain.QuoteModel
+import it.willuz.cleandroid.domain.displayName
 import it.willuz.cleandroid.util.quoted
 
 fun QuoteModel.asUiItem(): QuoteUiItem {
     return QuoteUiItem(this.id,
         this.quote.quoted(),
-        "${this.author?.firstName ?: ""} ${this.author?.lastName ?: ""}")
+        author?.displayName ?: "")
 }
